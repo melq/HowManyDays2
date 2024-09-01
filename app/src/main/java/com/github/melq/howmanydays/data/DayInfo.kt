f2272d3279eefef84f42298448f956661632eaa1
melq <droidlun@gmail.com>
2024-09-01 14:21:07 +0900

    データクラス作成

---
+++ app/src/main/java/com/github/melq/howmanydays/data/DayInfo.kt
@@ -0,0 +1,6 @@
+package com.github.melq.howmanydays.data
+
+import java.time.LocalDateTime
+import java.util.Date
+
+data class DayInfo(val title : String, val date: LocalDateTime, val displayMode: DisplayMode)
---
+++ app/src/main/java/com/github/melq/howmanydays/data/DisplayMode.kt
@@ -0,0 +1,11 @@
+package com.github.melq.howmanydays.data
+
+enum class DisplayMode {
+    SECONDS,
+    MINUTES,
+    HOURS,
+    DAYS,
+    WEEKS,
+    MONTHS,
+    YEARS
+}
