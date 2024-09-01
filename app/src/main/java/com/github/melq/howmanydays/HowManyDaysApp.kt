6e5945e8a27db07b6d5f5223ef3c1763ccbce5ff
melq <droidlun@gmail.com>
2024-09-01 15:14:06 +0900

    メイン画面作成

---
+++ app/src/main/java/com/github/melq/howmanydays/HowManyDaysApp.kt
@@ -0,0 +1,57 @@
+package com.github.melq.howmanydays
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.github.melq.howmanydays.ui.screens.MainScreen
+import com.github.melq.howmanydays.ui.screens.ScreenNames
+import com.github.melq.howmanydays.ui.theme.HowManyDaysTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HowManyDaysApp(
+) {
+    HowManyDaysTheme {
+        val navController = rememberNavController()
+        Scaffold(
+            topBar = {
+                TopAppBar( // TODO: 別Composableに切り出す
+                    colors = TopAppBarDefaults.largeTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    title = {
+                        Text(
+                            text = "TEST",
+                        )
+                    }
+                )
+            },
+            content = { padding ->
+                NavHost(
+                    modifier = Modifier.padding(padding),
+                    navController = navController,
+                    startDestination = ScreenNames.MainScreen.name
+                ) {
+                    composable(route = ScreenNames.MainScreen.name) {
+                        MainScreen(
+                            modifier = Modifier
+                        )
+                    }
+                    composable(route = ScreenNames.EditScreen.name) {
+                        // TODO: EditScreen
+                    }
+                }
+            }
+        )
+    }
+}
---
+++ app/src/main/java/com/github/melq/howmanydays/ui/screens/MainScreen.kt
@@ -0,0 +1,94 @@
+package com.github.melq.howmanydays.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.melq.howmanydays.data.DayInfo
+import com.github.melq.howmanydays.data.DisplayMode
+import com.github.melq.howmanydays.ui.theme.HowManyDaysTheme
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+@Composable
+fun MainScreen(
+    modifier: Modifier
+) {
+    HowManyDaysTheme {
+        Surface {
+            DaysList(modifier)
+        }
+    }
+}
+
+@Composable
+private fun DaysList(
+    modifier: Modifier
+) {
+    val days = emptyList<DayInfo>().toMutableList()
+    val displayModes = DisplayMode.entries.toTypedArray()
+    for ((index, displayMode) in displayModes.withIndex()) {
+        days += DayInfo(
+            "TestTitle${index}",
+            LocalDateTime.now().plusDays(index.toLong()),
+            displayMode
+        )
+    }
+
+    LazyColumn {
+        items(days) { day ->
+            DayItemRow(day, modifier)
+            HorizontalDivider(
+                modifier = Modifier
+                    .height(1.dp)
+                    .padding(8.dp, 0.dp),
+                color = Color.Gray
+            )
+        }
+    }
+}
+
+@Composable
+private fun DayItemRow(day: DayInfo, modifier: Modifier) {
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        onClick = { /*TODO*/ }) {
+        Column(
+            modifier = modifier
+                .padding(8.dp)
+        ) {
+            Text(
+                text = day.title,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(
+                text = day.date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DaysListPreview() {
+    HowManyDaysTheme {
+        Surface {
+            DaysList(modifier = Modifier)
+        }
+    }
+}
