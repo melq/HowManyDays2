27fb72fe15bdfefca1f5778b8da2aa85bf998bd9
melq <tu2sice@yahoo.co.jp>
2024-09-20 21:01:29 +0900

    fromOrdinal作成

--- app/src/main/java/com/github/melq/howmanydays/ui/screens/EditMode.kt
+++ app/src/main/java/com/github/melq/howmanydays/ui/screens/EditMode.kt
@@ -2,5 +2,15 @@ package com.github.melq.howmanydays.ui.screens
 
 enum class EditMode {
     Add,
-    Edit
+    Edit;
+
+    companion object {
+        fun fromOrdinal(ordinal: Int): EditMode {
+            return when (ordinal) {
+                0 -> Add
+                1 -> Edit
+                else -> throw IllegalArgumentException("Invalid ordinal: $ordinal")
+            }
+        }
+    }
 }
