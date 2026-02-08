783107decdb9808a55e50b2528333c508c7a25d3
melq <tu2sice@yahoo.co.jp>
2026-02-09 00:50:24 +0900

    fix: Title上の不要なスペースを削除 (Scaffoldのインセット調整)

--- app/src/main/java/com/github/melq/howmanydays/ui/screens/EditScreen.kt
+++ app/src/main/java/com/github/melq/howmanydays/ui/screens/EditScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
@@ -61,6 +62,7 @@ fun EditScreen(
     }
     HowManyDaysTheme {
         Scaffold(
+                contentWindowInsets = WindowInsets(0, 0, 0, 0),
                 bottomBar = {
                     val editedDayInfo =
                             DayInfo(
