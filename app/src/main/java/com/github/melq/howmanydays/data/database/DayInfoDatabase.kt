abffaf75e80e07664c984d5f044a55eb81fb5a78
melq <tu2sice@yahoo.co.jp>
2026-02-08 19:28:38 +0900

    fix: データベースのバージョン不整合によるクラッシュを修正

--- app/src/main/java/com/github/melq/howmanydays/data/database/DayInfoDatabase.kt
+++ app/src/main/java/com/github/melq/howmanydays/data/database/DayInfoDatabase.kt
@@ -13,7 +13,7 @@ import com.github.melq.howmanydays.data.entity.Converters
 import com.github.melq.howmanydays.data.entity.DayInfo
 import com.github.melq.howmanydays.data.entity.Milestone
 
-@Database(entities = [DayInfo::class, Milestone::class], version = 3, exportSchema = false)
+@Database(entities = [DayInfo::class, Milestone::class], version = 4, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class DayInfoDatabase : RoomDatabase() {
     abstract fun dayInfoDao(): DayInfoDao
@@ -30,7 +30,7 @@ abstract class DayInfoDatabase : RoomDatabase() {
                                         DayInfoDatabase::class.java,
                                         "day_info_database"
                                 )
-                                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+                                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4)
                                 .build()
                                 .also { Instance = it }
                     }
@@ -54,6 +54,13 @@ abstract class DayInfoDatabase : RoomDatabase() {
                         db.execSQL(
                                 "CREATE INDEX IF NOT EXISTS `index_milestone_dayInfoId` ON `milestone` (`dayInfoId`)"
                         )
+                    }
+                }
+
+        private val MIGRATION_3_4 =
+                object : Migration(3, 4) {
+                    override fun migrate(db: SupportSQLiteDatabase) {
+                        db.execSQL("DROP TABLE IF EXISTS `notified`")
                     }
                 }
     }
