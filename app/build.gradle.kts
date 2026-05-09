74bfd48abd8055c83c1481834547ac77e38e24e9
tsuzu <56225600+melq@users.noreply.github.com>
2026-05-09 18:28:56 +0900

    Release: Ver1.2 (#34)
    * chore: Ver1.2 リリースに向けてバージョンを更新
    
    * versionCodeを修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.melq.howmanydays"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
