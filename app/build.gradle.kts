3f098ee22c9abe1836593fd8a81946299668743a
melq <tu2sice@yahoo.co.jp>
2026-05-09 18:28:05 +0900

    versionCodeを修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.github.melq.howmanydays"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
